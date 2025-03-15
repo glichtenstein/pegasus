7b13167742cb2c03b8f3e3a147d101f706f20524
Gabriel Lichtenstein <gabriel.lichtenstein@gmail.com>
2025-03-15 01:38:14 -0400

    Update interactive job script to request 24 cores for 10 days and clarify usage instructions

--- eph/0_interactive_pegasus_job.sh
+++ eph/0_interactive_pegasus_job.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Description: This script is used to submit an interactive job to the Pegasus cluster.
-bsub -n 24 -q sccc -P sccc_hpc -Is bash
-
-# request 10 days of runtime
-#bsub -n 24 -q sccc -P sccc_hpc -W 240:00 -Is bash
+# It requests 24 cores for 10 days.
+# Usage: bash 0_interactive_pegasus_job.sh
+bsub -n 24 -q sccc -P sccc_hpc -W 240:00 -Is bash
