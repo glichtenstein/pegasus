62efcd21a9b06dcaee852557c24f8f8c4a1df987
Gabriel Lichtenstein <gabriel.lichtenstein@gmail.com>
2025-03-15 01:35:49 -0400

    Add comment to interactive job script for runtime request clarification

--- eph/0_interactive_pegasus_job.sh
+++ eph/0_interactive_pegasus_job.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Description: This script is used to submit an interactive job to the Pegasus cluster.
 bsub -n 24 -q sccc -P sccc_hpc -Is bash
+
+# request 10 days of runtime
+#bsub -n 24 -q sccc -P sccc_hpc -W 240:00 -Is bash
